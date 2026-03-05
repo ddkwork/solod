54d13d6e8cf768562f9c672db0967603e1b218cd
Anton <m@antonz.org>
2026-03-05 16:50:48 +0500

    so/c/assert

---
+++ so/c/assert/assert.go
@@ -0,0 +1,25 @@
+package assert
+
+import _ "embed"
+
+// Enabled reports whether assertions are enabled.
+// Cannot be changed at runtime.
+//
+//so:extern
+var Enabled bool
+
+//so:embed assert.h
+var assert_h string
+
+// Assert aborts the program if the condition is not true.
+// If [Enabled] is false, does nothing.
+//
+//so:extern
+func Assert(cond bool) {}
+
+// Assertf aborts the program with the given message
+// if the condition is not true.
+// If [Enabled] is false, does nothing.
+//
+//so:extern
+func Assertf(cond bool, msg string) {}
---
+++ testdata/c-assert/dst/main.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "so/builtin/builtin.h"
+#include "so/c/assert/assert.h"
+
+// -- Variables and constants --
