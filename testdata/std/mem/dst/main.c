bfe5b87acce1f489fe371b6fdeffd747c617f6f9
Anton <m@antonz.org>
2026-03-25 10:48:20 +0500

    testdata: merge std/mem with std/mem_arena

--- testdata/std/mem/dst/main.c
+++ testdata/std/mem/dst/main.c
@@ -87,5 +87,42 @@ int main(void) {
         so_String s2 = so_str("");
         mem_FreeString((mem_Allocator){0}, s2);
     }
-    withDefer();
+    {
+        // Free with defer.
+        withDefer();
+    }
+    {
+        // Arena allocator.
+        so_Slice buf = so_make_slice(so_byte, 1024, 1024);
+        mem_Arena arena = mem_NewArena(buf);
+        mem_Allocator a = (mem_Allocator){.self = &arena, .Alloc = mem_Arena_Alloc, .Free = mem_Arena_Free, .Realloc = mem_Arena_Realloc};
+        // Allocate a Point.
+        so_Result _res3 = mem_TryAlloc(main_Point, a);
+        main_Point* p = _res3.val.as_ptr;
+        so_Error err = _res3.err;
+        if (err != NULL) {
+            so_panic("initial allocation failed");
+        }
+        p->x = 11;
+        p->y = 22;
+        if (p->x != 11 || p->y != 22) {
+            so_panic("unexpected p.x or p.y");
+        }
+        // Free is a no-op.
+        mem_Free(main_Point, a, p);
+        // Reset and reallocate.
+        mem_Arena_Reset(&arena);
+        so_Result _res4 = mem_TryAlloc(main_Point, a);
+        main_Point* p2 = _res4.val.as_ptr;
+        err = _res4.err;
+        if (err != NULL) {
+            so_panic("allocation after reset failed");
+        }
+        // Memory should be zeroed.
+        if (p2->x != 0 || p2->y != 0) {
+            so_panic("memory not zeroed after reset");
+        }
+        p2->x = 33;
+        p2->y = 44;
+    }
 }
--- testdata/std/mem/src/main.go
+++ testdata/std/mem/src/main.go
@@ -86,5 +86,41 @@ func main() {
 		s2 := ""
 		mem.FreeString(nil, s2)
 	}
-	withDefer()
+	{
+		// Free with defer.
+		withDefer()
+	}
+	{
+		// Arena allocator.
+		buf := make([]byte, 1024)
+		arena := mem.NewArena(buf)
+		var a mem.Allocator = &arena
+
+		// Allocate a Point.
+		p, err := mem.TryAlloc[Point](a)
+		if err != nil {
+			panic("initial allocation failed")
+		}
+		p.x = 11
+		p.y = 22
+		if p.x != 11 || p.y != 22 {
+			panic("unexpected p.x or p.y")
+		}
+
+		// Free is a no-op.
+		mem.Free(a, p)
+
+		// Reset and reallocate.
+		arena.Reset()
+		p2, err := mem.TryAlloc[Point](a)
+		if err != nil {
+			panic("allocation after reset failed")
+		}
+		// Memory should be zeroed.
+		if p2.x != 0 || p2.y != 0 {
+			panic("memory not zeroed after reset")
+		}
+		p2.x = 33
+		p2.y = 44
+	}
 }
