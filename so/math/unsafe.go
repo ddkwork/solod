8d5d149363c1a48e2906248366403dda00b45f28
Anton <m@antonz.org>
2026-03-28 22:27:26 +0500

    so/math: use unsafe.Pointer for go compat

--- so/math/unsafe.go
+++ so/math/unsafe.go
@@ -4,24 +4,26 @@
 
 package math
 
+import "unsafe"
+
 // Float32bits returns the IEEE 754 binary representation of f,
 // with the sign bit of f and the result in the same bit position.
 // Float32bits(Float32frombits(x)) == x.
-func Float32bits(f float32) uint32 { return *(any(&f).(*uint32)) }
+func Float32bits(f float32) uint32 { return *(*uint32)(unsafe.Pointer(&f)) }
 
 // Float32frombits returns the floating-point number corresponding
 // to the IEEE 754 binary representation b, with the sign bit of b
 // and the result in the same bit position.
 // Float32frombits(Float32bits(x)) == x.
-func Float32frombits(b uint32) float32 { return *(any(&b).(*float32)) }
+func Float32frombits(b uint32) float32 { return *(*float32)(unsafe.Pointer(&b)) }
 
 // Float64bits returns the IEEE 754 binary representation of f,
 // with the sign bit of f and the result in the same bit position,
 // and Float64bits(Float64frombits(x)) == x.
-func Float64bits(f float64) uint64 { return *(any(&f).(*uint64)) }
+func Float64bits(f float64) uint64 { return *(*uint64)(unsafe.Pointer(&f)) }
 
 // Float64frombits returns the floating-point number corresponding
 // to the IEEE 754 binary representation b, with the sign bit of b
 // and the result in the same bit position.
 // Float64frombits(Float64bits(x)) == x.
-func Float64frombits(b uint64) float64 { return *(any(&b).(*float64)) }
+func Float64frombits(b uint64) float64 { return *(*float64)(unsafe.Pointer(&b)) }
