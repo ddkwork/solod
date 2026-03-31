cd8ffc4a8e238b0d61d2806c68046eae14e3d289
Anton <m@antonz.org>
2026-03-31 06:54:17 +0500

    so/mem: NoAlloc

---
+++ so/mem/noalloc.go
@@ -0,0 +1,25 @@
+package mem
+
+import "solod.dev/so/errors"
+
+var ErrNoAlloc = errors.New("mem: allocation not allowed")
+
+// NoAlloc is an instance of an Allocator that returns an error
+// on any allocation. Free is a no-op.
+// It's meant for cases when allocations are strictly forbidden.
+var NoAlloc Allocator = &NoAllocator{}
+
+// NoAllocator is an Allocator that returns an error
+// on any allocation. Free is a no-op.
+// It's meant for cases when allocations are strictly forbidden.
+type NoAllocator struct{}
+
+func (*NoAllocator) Alloc(size int, align int) (any, error) {
+	return nil, ErrNoAlloc
+}
+
+func (*NoAllocator) Realloc(ptr any, oldSize int, newSize int, align int) (any, error) {
+	return nil, ErrNoAlloc
+}
+
+func (*NoAllocator) Free(ptr any, size int, align int) {}
